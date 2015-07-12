73d707eeae9a84b181ab0c986e170898d8b40a6f
Kenta <oono@preferred.jp>
2015-07-12 18:56:23 +0900

    Disable messages of scripts

--- ami/cpu/admin/docker_start.sh
+++ ami/cpu/admin/docker_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(docker run -d -p 22 --name=caffe_chainer delta2323/jnns2015-tutorial && sleep 2) || (docker start caffe_chainer && sleep 2)
+(docker run -d -p 22 --name=caffe_chainer delta2323/jnns2015-tutorial && sleep 2) >/dev/null 2>&1 || (docker start caffe_chainer && sleep 2) >/dev/null 2>&1
--- ami/cpu/admin/docker_stop.sh
+++ ami/cpu/admin/docker_stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker stop caffe_chainer
-docker rm caffe_chainer
+docker stop caffe_chainer >/dev/null 2>&1
+docker rm caffe_chainer >/dev/null 2>&1
